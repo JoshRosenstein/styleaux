c433271352189bfc945fc7cd1ac84c816d25a31c
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-11-15 20:48:48 -0500

    [helper-plugin-utils]: add some shared types

--- packages/styleaux-helper-plugin-utils/index.ts
+++ packages/styleaux-helper-plugin-utils/index.ts
@@ -1,3 +1,4 @@
 export * from './utils'
 export * from './getAndCheckMethod'
 export * from './constants'
+export * from './types'
---
+++ packages/styleaux-helper-plugin-utils/types.ts
@@ -0,0 +1,15 @@
+export type Nothing = null | undefined
+export type Maybe<T> = Nothing | T
+
+export type Primitive = string | number | boolean | undefined | null
+export type Dict<T, K extends string | number = string> = {[key in K]: T}
+export type AnyDict = Dict<any>
+export type MaybeAnyDict = Maybe<AnyDict>
+export type DictValues<T> = T extends Dict<infer U> ? U : never
+export type DeepPartial<T> = {
+  [P in keyof T]?: T[P] extends Array<infer U>
+    ? Array<DeepPartial<U>>
+    : T[P] extends ReadonlyArray<infer U>
+    ? ReadonlyArray<DeepPartial<U>>
+    : DeepPartial<T[P]>
+}
