4d4cb4ca3a25d110b71527b3de326ced547453c5
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-11-15 20:47:02 -0500

    configs

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  // transform: {
-  //   //  '\\.css$': '<rootDir>/test/styleTransform.js',
-  //   '^.+\\.js?$': 'babel-jest'
-  // },
-  clearMocks: true,
+  transform: {
+    '\\.ts$': 'ts-jest',
+  },
+  watchPlugins: ['jest-watch-lerna-packages'],
   testEnvironment: 'node',
   collectCoverageFrom: ['{packages}/**/*.js'],
-  modulePathIgnorePatterns: ['/__fixtures__/'],
-  roots: ['<rootDir>/packages'],
-  testRunner: 'jest-circus/runner',
+  //  modulePathIgnorePatterns: ['/__fixtures__/'],
+  // roots: ['<rootDir>/packages'],
+  //testRunner: 'jest-circus/runner',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
 }
