cb6f1c342f158543366deec6aaa597ad339b4a95
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2019-03-19 00:01:49 -0400

    warn-once

---
+++ packages/styleaux-core/src/utils/warn-once.ts
@@ -0,0 +1,16 @@
+export function createWarnOnce(title) {
+  if (process.env.NODE_ENV !== "production") {
+    let printed = {};
+
+    return function warnOnce(...args: any[]) {
+      const M=JSON.stringify(args)
+      if (printed[M]) return;
+      printed[M] = true;
+
+      if (typeof console !== "undefined" && console.warn)
+        console.warn(`[${title}]:`, ...args);
+    };
+  }
+
+  return (...args: any[]) => {};
+}
