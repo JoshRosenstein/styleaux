e0e1b3158e6960f870a24d22c843512aff9fdcc7
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2019-03-18 22:59:43 -0400

    init constants

---
+++ packages/styleaux-core/src/constants.ts
@@ -0,0 +1,31 @@
+
+export type THEME_KEY='theme'
+export const THEME_KEY = "theme";
+
+export type DEFAULT_KEY='theme'
+export const DEFAULT_KEY = "default";
+
+export type MEDIA_KEY='media'
+export const MEDIA_KEY = "media"
+
+export type DEFAULT_MEDIA_KEY='all'
+export const DEFAULT_MEDIA_KEY = "all";
+
+export type STATIC_STYLES_KEY='_staticStyles'
+export const STATIC_STYLES_KEY='_staticStyles'
+
+export type STYLES_KEY='styles'
+export const STYLES_KEY='styles'
+
+export type STYLE_PROPS_KEY='_styleProps'
+export const STYLE_PROPS_KEY='_styleProps'
+
+export enum CONSTANTS{
+    DEFAULT_KEY = "default",
+    THEME_KEY = "theme",
+    MEDIA_KEY = "media",
+    DEFAULT_MEDIA_KEY = "all",
+    STYLES_KEY='styles',
+    STATIC_STYLES_KEY='_staticStyles',
+    STYLE_PROPS_KEY='_styleProps',
+}
