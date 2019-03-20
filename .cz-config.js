f8165c6e95ef9be2c4a97f4ae053654b89f8984c
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2019-03-20 07:54:10 -0400

    chore:     Build process or auxiliary tool changes(mono):

--- .cz-config.js
+++ .cz-config.js
@@ -1,45 +1,73 @@
-'use strict'
-
 module.exports = {
+
+
+
   types: [
-    {value: 'feat', name: 'feat:     A new feature'},
-    {value: 'fix', name: 'fix:      A bug fix'},
-    {value: 'docs', name: 'docs:     Documentation only changes'},
+
+    { chore: '🤖chore', name: 'chore:     Build process or auxiliary tool changes' },
+    { value: '🎸feat', name: 'feat:     A new feature' },
+    { value: '🐛fix', name: 'fix:      A bug fix' },
+    { value: '✏️docs', name: 'docs:     Documentation only changes' },
     {
-      value: 'style',
+      value: '💄style',
       name:
         'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
     },
     {
-      value: 'refactor',
-      name:
-        'refactor: A code change that neither fixes a bug nor adds a feature',
+      value: '💡refactor',
+      name: 'refactor:     A code change that neither fixes a bug nor adds a feature',
     },
     {
-      value: 'perf',
+      value: '⚡️perf',
       name: 'perf:     A code change that improves performance',
     },
-    {value: 'test', name: 'test:     Adding missing tests'},
+    { value: ':neckbeard:test', name: 'test:     Adding missing tests' },
     {
-      value: 'chore',
+      value: '🤖chore',
       name:
         'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation',
     },
-    {value: 'revert', name: 'revert:   Revert to a commit'},
+    { value: '😵revert', name: 'revert:   Revert to a commit' },
+    { value: '🙏WIP', name: 'WIP:      Work in progress' },
   ],
 
-  scopes: [
-    {name: 'styleaux-core'},
-    {name: 'styleaux-docs'},
-    {name: 'styleaux-system'},
-  ],
+  scopes: [{ name: 'mono' }, { name: 'core' },{ name: 'styles-base' },{ name: 'generators' }],
+
+  allowTicketNumber: false,
+  isTicketNumberRequired: false,
+  ticketNumberPrefix: 'TICKET-',
+  ticketNumberRegExp: '\\d{1,5}',
 
   // it needs to match the value for field type. Eg.: 'fix'
+  /*
   scopeOverrides: {
-    chore: [],
+    fix: [
+
+      {name: 'merge'},
+      {name: 'style'},
+      {name: 'e2eTest'},
+      {name: 'unitTest'}
+    ]
+  },
+  */
+  // override the messages, defaults are as follows
+  messages: {
+    type: "Select the type of change that you're committing:",
+    scope: '\nDenote the SCOPE of this change (optional):',
+    // used if allowCustomScopes is true
+    customScope: 'Denote the SCOPE of this change:',
+    subject: 'Write a short, imperative mood description of the change:\n',
+    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+    breaking: 'List any BREAKING CHANGES (optional):\n',
+    footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
+    confirmCommit: 'Are you sure you want to proceed with the commit above?',
   },
 
   allowCustomScopes: true,
   allowBreakingChanges: ['feat', 'fix'],
-  appendBranchNameToCommitMessage: false,
-}
+  // skip any questions you want
+  skipQuestions: ['body', 'footer'],
+
+  // limit subject length
+  subjectLimit: 100,
+};
