268d3a3814d60a0ef2da2fe5399f003973600091
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2019-03-27 16:44:38 -0400

    🎨format(generators):

--- packages/styleaux-generators/src/palette.ts
+++ packages/styleaux-generators/src/palette.ts
@@ -6,7 +6,66 @@ interface Chroma extends ChromaStatic{
 }
 const chroma=require('chroma-js') as Chroma
 
-const hueName = (h:number, s:number, l:number) => {
+
+
+
+/**
+ * Generates a color pallette
+ *
+ *@link getThemePathOr
+ *
+  * @param options Generator Options.
+  * @param options.hueOffset number between 0-30
+  * @param options.invertedLightness enable for automatic Dark Mode
+  * @param options.highContrastMode enable for accessibility
+  * @param options.highContrastLightnessScale // default [1-10]
+  * @param options.lowContrastLightnessScale
+  * @param options.hues hues- calculated from hueOffset and scales
+ */
+
+
+export const generatePalette = ({
+  hueOffset= 28,
+  invertedLightness=false,
+  highContrastMode=false,
+  highContrastLightnessScale = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  lowContrastLightnessScale = highContrastLightnessScale.map(
+    x => x * 0.8 + 1.5
+  ),
+  hues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(_ => _ * 30 + hueOffset)
+}:{
+  hueOffset?:number,
+  invertedLightness?:boolean,
+  highContrastMode?:boolean,
+  highContrastLightnessScale?:number[]
+  lowContrastLightnessScale?:number[],
+  hues?:number[]
+
+}):Partial<Record<ReturnType<typeof hueName>,string[]>> => {
+  const lightnessScale = (highContrastMode
+    ? highContrastLightnessScale
+    : lowContrastLightnessScale
+  ).map(_ => _ * 10);
+
+  if (invertedLightness) {
+    lightnessScale.reverse();
+  }
+
+  return hues.reduce(
+    (colors, hue) => {
+      colors[hueName(hue, 0.75, 0.5)] = lightnessScale.map(lightness =>
+        chroma.hcl(hue, 85 - (20 * lightness) / 100, lightness).hex()
+      );
+      return colors;
+    },
+    {
+      gray: lightnessScale.map(lightness => chroma.hcl(0, 0, lightness).hex())
+    }
+  );
+};
+
+
+function hueName (h:number, s:number, l:number)  {
   const i = Math.round(h) % 360;
 
   if (s < 0.05 || (l < 0.12 && s < 0.18) || l > 0.95) {
@@ -70,60 +129,4 @@ const hueName = (h:number, s:number, l:number) => {
   }
 
   return 'red';
-};
-
-
-/**
- * Generates a color pallette
- *
- *@link getThemePathOr
- *
-  * @param options Generator Options.
-  * @param options.hueOffset number between 0-30
-  * @param options.invertedLightness enable for automatic Dark Mode
-  * @param options.highContrastMode enable for accessibility
-  * @param options.highContrastLightnessScale // default [1-10]
-  * @param options.lowContrastLightnessScale
-  * @param options.hues hues- calculated from hueOffset and scales
- */
-
-
-export const generatePalette = ({
-  hueOffset,
-  invertedLightness=false,
-  highContrastMode=false,
-  highContrastLightnessScale = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-  lowContrastLightnessScale = highContrastLightnessScale.map(
-    x => x * 0.8 + 1.5
-  ),
-  hues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(_ => _ * 30 + hueOffset)
-}:{
-  hueOffset:number,
-  invertedLightness?:boolean,
-  highContrastMode?:boolean,
-  highContrastLightnessScale?:number[]
-  lowContrastLightnessScale?:number[],
-  hues?:number[]
-
-}):Partial<Record<ReturnType<typeof hueName>,string[]>> => {
-  const lightnessScale = (highContrastMode
-    ? highContrastLightnessScale
-    : lowContrastLightnessScale
-  ).map(_ => _ * 10);
-
-  if (invertedLightness) {
-    lightnessScale.reverse();
-  }
-
-  return hues.reduce(
-    (colors, hue) => {
-      colors[hueName(hue, 0.75, 0.5)] = lightnessScale.map(lightness =>
-        chroma.hcl(hue, 85 - (20 * lightness) / 100, lightness).hex()
-      );
-      return colors;
-    },
-    {
-      gray: lightnessScale.map(lightness => chroma.hcl(0, 0, lightness).hex())
-    }
-  );
 };
